6fea0359448809ed88e1ca798eeb7092b2bc966f
zaidafk <zaidarschool@gmail.com>
2026-09-18 23:02:36 +0800

    Add files via upload

---
+++ C/PRAK004-2610820310009-Zaid_Al_Hibbur_Rasul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("##############################\n");
+    printf("#                            #\n");
+    printf("#    Zaid Al Hibbur Rasul    #\n");
+    printf("#       2610820310009        #\n");
+    printf("#                            #\n");
+    printf("##############################\n");
+    return 0;
+}
